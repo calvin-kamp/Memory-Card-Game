a623f0afda610ff3e194bee4683b165931fea756
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-01-12 10:40:18 +0100

    chore: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './styles/main.scss'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { initThemeStore } from '@stores/theme-store'
+import { initGamePage } from '@pages/game.page'
+import { initSettingsPage } from '@pages/settings.page'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+initThemeStore()
+initSettingsPage()
+initGamePage()
